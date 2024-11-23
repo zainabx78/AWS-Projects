476ffa7d8c77736de1b4e886b1c7198f08f88d3f
zainabx78 <zainabfarooq001@gmail.com>
2024-11-23 19:01:17 +0000

    Add files via upload

---
+++ AWS.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Author: Zainab Farooq
+#Date: 23/11/2024
+#V1
+#A script to gain information on AWS services: S3, EC2, IAM
+#Prerequisite--> have AWS CLI installed and run AWS configure in your terminal. 
+
