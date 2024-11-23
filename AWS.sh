70268f2bb234085c5de8e52e6bbb80027c879b9f
zainabx78 <zainabfarooq001@gmail.com>
2024-11-23 19:12:42 +0000

    Update AWS.sh

--- AWS.sh
+++ AWS.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-
 #Author: Zainab Farooq
 #Date: 23/11/2024
 #V1
-#A script to gain information on AWS services: S3, EC2, IAM
+#A simple bash script to gain information on AWS services: S3, EC2, IAM, Lambda.
 #Prerequisite--> have AWS CLI installed and run AWS configure in your terminal. 
 
+#run in debug mode
+set -x
+
+#list s3 buckets
+aws s3 ls
+
+#list ec2 instance ID's only.
+aws ec2 describe-instances | jq 'reservations [].Instances[].InstanceId'
+
+#list lambda functions
+aws lambda list-functions
+
+#list iam users
+aws iam list-users
